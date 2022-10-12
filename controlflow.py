6b3cf9acf292826b66f8a0e0e5f6afc883f3251d
AbisheK0726 <abishekaneese726@gmail.com>
2022-10-12 14:22:07 +0100

    if-else statments

---
+++ controlflow.py
@@ -0,0 +1,12 @@
+# use if statmenets to check the weather and print a message
+weather = input("What is the weather like today? ")
+
+if weather == "sunny":
+    print("Wear a t-shirt")
+elif weather == "rainy":
+    print ("Wear a raincoat")
+elif weather == "snowy":
+    print ("Wear a coat and boots")
+else:
+    print ("Wear whatever you want")
+    
